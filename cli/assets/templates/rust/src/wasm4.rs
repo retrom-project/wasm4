12e0296c04eabf755dbf69662303a42f54f8c90d
Matt Harding <majaharding@gmail.com>
2026-08-09 05:19:31 +0100

    Fix link errors in Rust template code
    Fixes the linking errors in the Rust example in recent versions of the Rust compiler by adding:
    `#[link(wasm_import_module = "env")]`
    to the extern block.
    
    Also, consolidates most of the externs into a single block.

--- cli/assets/templates/rust/src/wasm4.rs
+++ cli/assets/templates/rust/src/wasm4.rs
@@ -55,10 +55,6 @@ pub const SYSTEM_HIDE_GAMEPAD_OVERLAY: u8 = 2;
 pub fn blit(sprite: &[u8], x: i32, y: i32, width: u32, height: u32, flags: u32) {
     unsafe { extern_blit(sprite.as_ptr(), x, y, width, height, flags) }
 }
-unsafe extern "C" {
-    #[link_name = "blit"]
-    fn extern_blit(sprite: *const u8, x: i32, y: i32, width: u32, height: u32, flags: u32);
-}
 
 /// Copies a subregion within a larger sprite atlas to the framebuffer.
 #[allow(clippy::too_many_arguments)]
@@ -87,20 +83,6 @@ pub fn blit_sub(
         )
     }
 }
-unsafe extern "C" {
-    #[link_name = "blitSub"]
-    fn extern_blit_sub(
-        sprite: *const u8,
-        x: i32,
-        y: i32,
-        width: u32,
-        height: u32,
-        src_x: u32,
-        src_y: u32,
-        stride: u32,
-        flags: u32,
-    );
-}
 
 pub const BLIT_2BPP: u32 = 1;
 pub const BLIT_1BPP: u32 = 0;
@@ -112,37 +94,21 @@ pub const BLIT_ROTATE: u32 = 8;
 pub fn line(x1: i32, y1: i32, x2: i32, y2: i32) {
     unsafe { extern_line(x1, y1, x2, y2) }
 }
-unsafe extern "C" {
-    #[link_name = "line"]
-    fn extern_line(x1: i32, y1: i32, x2: i32, y2: i32);
-}
 
 /// Draws an oval (or circle).
 pub fn oval(x: i32, y: i32, width: u32, height: u32) {
     unsafe { extern_oval(x, y, width, height) }
-}
-unsafe extern "C" {
-    #[link_name = "oval"]
-    fn extern_oval(x: i32, y: i32, width: u32, height: u32);
 }
 
 /// Draws a rectangle.
 pub fn rect(x: i32, y: i32, width: u32, height: u32) {
     unsafe { extern_rect(x, y, width, height) }
 }
-unsafe extern "C" {
-    #[link_name = "rect"]
-    fn extern_rect(x: i32, y: i32, width: u32, height: u32);
-}
 
 /// Draws text using the built-in system font.
 pub fn text<T: AsRef<[u8]>>(text: T, x: i32, y: i32) {
     let text_ref = text.as_ref();
     unsafe { extern_text(text_ref.as_ptr(), text_ref.len(), x, y) }
-}
-unsafe extern "C" {
-    #[link_name = "textUtf8"]
-    fn extern_text(text: *const u8, length: usize, x: i32, y: i32);
 }
 
 /// Draws a vertical line
@@ -152,21 +118,11 @@ pub fn vline(x: i32, y: i32, len: u32) {
     }
 }
 
-unsafe extern "C" {
-    #[link_name = "vline"]
-    fn extern_vline(x: i32, y: i32, len: u32);
-}
-
 /// Draws a horizontal line
 pub fn hline(x: i32, y: i32, len: u32) {
     unsafe {
         extern_hline(x, y, len);
     }
-}
-
-unsafe extern "C" {
-    #[link_name = "hline"]
-    fn extern_hline(x: i32, y: i32, len: u32);
 }
 
 // ┌───────────────────────────────────────────────────────────────────────────┐
@@ -178,10 +134,6 @@ unsafe extern "C" {
 /// Plays a sound tone.
 pub fn tone(frequency: u32, duration: u32, volume: u32, flags: u32) {
     unsafe { extern_tone(frequency, duration, volume, flags) }
-}
-unsafe extern "C" {
-    #[link_name = "tone"]
-    fn extern_tone(frequency: u32, duration: u32, volume: u32, flags: u32);
 }
 
 pub const TONE_PULSE1: u32 = 0;
@@ -202,6 +154,7 @@ pub const TONE_NOTE_MODE: u32 = 64;
 // │                                                                           │
 // └───────────────────────────────────────────────────────────────────────────┘
 
+#[link(wasm_import_module = "env")]
 unsafe extern "C" {
     /// Reads up to `size` bytes from persistent storage into the pointer `dest`.
     pub fn diskr(dest: *mut u8, size: u32) -> u32;
@@ -221,7 +174,48 @@ pub fn trace<T: AsRef<str>>(text: T) {
     let text_ref = text.as_ref();
     unsafe { extern_trace(text_ref.as_ptr(), text_ref.len()) }
 }
+
+// ─────────────────────────────────────────────────────────────────────────────
+
+#[link(wasm_import_module = "env")]
 unsafe extern "C" {
+    #[link_name = "blit"]
+    fn extern_blit(sprite: *const u8, x: i32, y: i32, width: u32, height: u32, flags: u32);
+
+    #[link_name = "blitSub"]
+    fn extern_blit_sub(
+        sprite: *const u8,
+        x: i32,
+        y: i32,
+        width: u32,
+        height: u32,
+        src_x: u32,
+        src_y: u32,
+        stride: u32,
+        flags: u32,
+    );
+
+    #[link_name = "line"]
+    fn extern_line(x1: i32, y1: i32, x2: i32, y2: i32);
+
+    #[link_name = "oval"]
+    fn extern_oval(x: i32, y: i32, width: u32, height: u32);
+
+    #[link_name = "rect"]
+    fn extern_rect(x: i32, y: i32, width: u32, height: u32);
+
+    #[link_name = "textUtf8"]
+    fn extern_text(text: *const u8, length: usize, x: i32, y: i32);
+
+    #[link_name = "vline"]
+    fn extern_vline(x: i32, y: i32, len: u32);
+
+    #[link_name = "hline"]
+    fn extern_hline(x: i32, y: i32, len: u32);
+
+    #[link_name = "tone"]
+    fn extern_tone(frequency: u32, duration: u32, volume: u32, flags: u32);
+
     #[link_name = "traceUtf8"]
     fn extern_trace(trace: *const u8, length: usize);
 }
